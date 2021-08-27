2c1a1d95cc09ff08628153d2ce8718981fbf1628
Denis Dickson <denisdschirmer@gmail.com>
2021-08-27 00:51:13 +0000

    Aulas finais

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import sqlite3
-from flask import Flask, request, session, g, redirect, abort, render_template, flash
+from flask import Flask, request, url_for, session, g, redirect, abort, render_template, flash
 
 # configuração
 DATABASE = "blog.db"
-SECRET_KEY = "pudim"
+SECRET_KEY = 'pudim'
+
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -15,7 +16,7 @@ def conectar_bd():
 
 @app.before_request
 def antes_requisicao():
-    g.bd = conectar_bd()  # g var global do flask, deixa acessível dentro da aplicação
+    g.bd = conectar_bd()
 
 
 @app.teardown_request
@@ -24,10 +25,41 @@ def depois_request(exc):
 
 
 @app.route('/')
+@app.route('/entradas')
 def exibir_entradas():
-    return render_template('exibir_entradas.html')
+    sql = "SELECT titulo, text FROM entradas ORDER BY id DESC"
+    cur = g.bd.execute(sql)
+    entradas = []
+    for titulo, text in cur.fetchall():
+        entradas.append({'titulo': titulo, 'text': text})
+    return render_template('exibir_entradas.html', entradas=entradas)
 
 
-@app.route('/hello')
-def pagina_inicial():
-    return "Hello World"
+@app.route('/inserir')
+def inserir_entrada():
+    if not session.get('logado'):
+        abort(401)
+    sql = "INSERT INTO entradas(titulo, text) VALUES (?,?)"
+    g.bd.execute(sql, request.form['campoTitulo'], request.form['campoTexto'])
+    g.bd.commit()
+    return redirect(url_for('exibir_entradas'))
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logado', None)
+    return redirect(url_for('exibr_entradas'))
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    erro = None
+    if request.method == "POST":
+        if request.form['campoUsuario'] != 'admin' \
+                or request.form['campoSenha'] != 'admin':
+            erro = "Senha ou usuário inválidos !"
+        else:
+            session['logado'] = True
+        print("Foi post")
+        return redirect(url_for('exibir_entradas'))
+    return render_template('login.html', erro=erro)
